6de44a3dd522c50d56bebce57f188281ba6f3bcb
Den Yatsenko <denyatsenko@outlook.com>
2022-09-25 08:53:28 +0300

    wip

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -7,12 +7,23 @@ use App\Models\Game;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class GameController extends Controller
 {
-    public function show(string $slug): Factory|View|Application
+    public function show(string $slug, Request $request): Factory|View|Application
     {
-        return view('game.show', ['game' => Game::firstWhere('slug', $slug)]);
+        $game = Game::firstWhere('slug', $slug);
+
+        $userTrackedGames = $request->user()->trackedGames;
+
+        $tracking = $userTrackedGames->contains(function ($userTrackedGames) use ($game) {
+            if ($userTrackedGames->game_id === $game->id) {
+                return true;
+            }
+        });
+
+        return view('game.show', ['game' => $game, 'tracking' => $tracking]);
     }
 }
